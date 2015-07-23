a0b2c4d6307fab22edd7510963d96abb693b5ec6
tiagofer <tiago.fer1987@gmail.com>
2015-07-23 12:49:16 -0300

    version-0.3:Stop Camera in the end

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform person;
+	public float smooth = 0.2f;
+	public float vel = 0.3f;
+	public float velY = 0.5f;
+	public float posX;
+	public float posY;
+	private bool _isFollow;
+	Vector3 offset;
+
+
+	void Start() 
+	{
+		offset = transform.position - person.position;
+		_isFollow = true;
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		//posX = Mathf.SmoothDamp(transform.position.x, person.position.x, ref vel, smooth);
+		//posY = Mathf.SmoothDamp(transform.position.y, person.position.y, ref velY, smooth);
+		if (person.position.x >= -4.7f) {
+			_isFollow = true;
+		} else 
+		{
+			_isFollow = false;
+		}
+		if (_isFollow) {
+			Vector3 targetCamPos = person.position + offset;
+			transform.position = Vector3.Lerp (targetCamPos, transform.position, smooth * Time.deltaTime);
+		} 
+	
+	}
+}
