9e31376450edd18a078eeb5558cb5df7499f67ca
tiagofer <tiago.fer1987@gmail.com>
2015-07-22 22:57:34 -0300

    version-0.2:Define end Scene

--- Assets/Scripts/Camera/Camera.cs
+++ Assets/Scripts/Camera/Camera.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class Camera : MonoBehaviour {
 
 	public Transform person;
-	public float smooth = 0.3f;
+	public float smooth = 0.2f;
 	public float vel = 0.3f;
 	public float velY = 0.5f;
 	public float posX;
 	public float posY;
-	// Update is called once per frame
-	void Update () {
-		posX = Mathf.SmoothDamp(transform.position.x, person.position.x, ref vel, smooth);
-		posY = Mathf.SmoothDamp(transform.position.y, person.position.y, ref velY, smooth);
+	Vector3 offset;
 
-		transform.position = new Vector3 (posX, posY, transform.position.z);
+
+	void Start() 
+	{
+		offset = transform.position - person.position;
+
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		//posX = Mathf.SmoothDamp(transform.position.x, person.position.x, ref vel, smooth);
+		//posY = Mathf.SmoothDamp(transform.position.y, person.position.y, ref velY, smooth);
+		Vector3 targetCamPos = person.position + offset;
+		transform.position = Vector3.Lerp (targetCamPos, transform.position, smooth * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/Player/DetectEndScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectEndScene : MonoBehaviour {
+
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("yeti") || other.gameObject.CompareTag("Player"))
+		{
+			gameObject.GetComponent<Player>().velocity = 0.0f;
+			gameObject.GetComponent<Yeti>().velocity = 0.0f;
+		}
+	}
+}
